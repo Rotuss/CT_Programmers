6571c0be1eafdce01e392368b66331c2d6647752
Rotus <12rotus31@gmail.com>
2023-09-04 19:16:28 +0900

    Lv2 카펫

--- Programmers/Level2/ProgrammersLevel2.cpp
+++ Programmers/Level2/ProgrammersLevel2.cpp
@@ -6,32 +6,61 @@
 using namespace std;
 
 // 짝지어 제거하기
-int solution(string s)
-{
-    int answer = -1;
-    stack<char> stackchar;
-    
-    for (int i = 0; i < s.size(); ++i)
+//int solution(string s)
+//{
+//    int answer = -1;
+//    stack<char> stackchar;
+//    
+//    for (int i = 0; i < s.size(); ++i)
+//    {
+//        if (true != stackchar.empty() && s[i] == stackchar.top())
+//        {
+//            stackchar.pop();
+//        }
+//        else
+//        {
+//            stackchar.push(s[i]);
+//        }
+//    }
+//    
+//    // 짝지어 제거 모두 성공 1, 실패 0
+//    return answer = stackchar.empty();
+//}
+
+// 카펫
+vector<int> solution(int brown, int yellow) {
+    vector<int> answer;
+    int iYX = 0;
+    int iYY = 0;
+
+    for (int i = 1; i < yellow; ++i)
     {
-        if (true != stackchar.empty() && s[i] == stackchar.top())
+        if (0 == yellow % i)
         {
-            stackchar.pop();
-        }
-        else
-        {
-            stackchar.push(s[i]);
+            iYX = yellow / i;
+            iYY = i;
+
+            if (brown == (iYX + 2) * 2 + iYY * 2)
+                break;
         }
     }
-    
-    // 짝지어 제거 모두 성공 1, 실패 0
-    return answer = stackchar.empty();
+
+    answer.push_back(iYX + 2);
+    answer.push_back(iYY + 2);
+
+    return answer;
 }
 
 int main(void)
 {
     // 짝지어 제거하기
-    solution("baabaa");
-    solution("cdcd");
+    /*solution("baabaa");
+    solution("cdcd");*/
+
+    // 카펫
+    solution(10, 2);
+    solution(8, 1);
+    solution(24, 24);
 
     return 0;
 }
