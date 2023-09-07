798b621e61fa005cd02e98c9c970e1fea648c409
Rotus <12rotus31@gmail.com>
2023-09-07 19:10:12 +0900

    Lv2 프로세스

--- Programmers/Level2/ProgrammersLevel2.cpp
+++ Programmers/Level2/ProgrammersLevel2.cpp
@@ -114,52 +114,102 @@ using namespace std;
 //}
 
 // 기능개발
-vector<int> solution(vector<int> progresses, vector<int> speeds)
+//vector<int> solution(vector<int> progresses, vector<int> speeds)
+//{
+//    vector<int> answer;
+//    queue<int> qHandout;
+//    int iDay = 0;
+//    int iCheck = 0;
+//    int iCount = 0;
+//
+//    // 남은 일자 계산
+//    for (int i = 0; i < progresses.size(); ++i)
+//    {
+//        iDay = (100 - progresses[i]) / speeds[i];
+//
+//        if (0 != (100 - progresses[i]) % speeds[i])
+//        {
+//            ++iDay;
+//        }
+//
+//        qHandout.push(iDay);
+//    }
+//
+//    // 첫 배출 정보 및 수
+//    iCheck = qHandout.front();
+//    qHandout.pop();
+//    ++iCount;
+//
+//    // 배출 카운트 및 검토
+//    while (0 <= qHandout.size())
+//    {
+//        // 모든 배출일자가 빠지면 남은 카운트 추가
+//        if (0 == qHandout.size())
+//        {
+//            answer.push_back(iCount);
+//            break;
+//        }
+//        else if (iCheck >= qHandout.front())
+//        {
+//            ++iCount;
+//            qHandout.pop();
+//        }
+//        else
+//        {
+//            answer.push_back(iCount);
+//            iCheck = qHandout.front();
+//            qHandout.pop();
+//            iCount = 1;
+//        }
+//    }
+//
+//    return answer;
+//}
+
+// 프로세스
+int solution(vector<int> priorities, int location)
 {
-    vector<int> answer;
-    queue<int> qHandout;
-    int iDay = 0;
-    int iCheck = 0;
-    int iCount = 0;
+    int answer = 1;
+    int iIndex = 0;
+    int iLocationIndex = location;
 
-    // 남은 일자 계산
-    for (int i = 0; i < progresses.size(); ++i)
+    // 중요도 순서 큐에 저장
+    queue<int> qProcess;
+    for (int i = 0; i < priorities.size(); ++i)
     {
-        iDay = (100 - progresses[i]) / speeds[i];
-
-        if (0 != (100 - progresses[i]) % speeds[i])
-        {
-            ++iDay;
-        }
-
-        qHandout.push(iDay);
+        qProcess.push(priorities[i]);
     }
 
-    // 첫 배출 정보 및 수
-    iCheck = qHandout.front();
-    qHandout.pop();
-    ++iCount;
+    // 중요도 순서 내림차순으로 정렬
+    sort(priorities.begin(), priorities.end(), greater<>());
 
-    // 배출 카운트 및 검토
-    while (0 <= qHandout.size())
+    while (true)
     {
-        // 모든 배출일자가 빠지면 남은 카운트 추가
-        if (0 == qHandout.size())
+        // 내림차순으로 정렬된 순서와 큐의 제일 앞에 있는 수 같은지 비교
+        // 같지 않다면 빼서 뒤로 삽입
+        // 위치도 같이 변경(위치인덱스가 0이라면 큐 사이즈의 -1 한만큼, 즉 제일 뒷 인덱스 번호 부여. 0이 아니라면 현재 인덱스에서 -1)
+        if (priorities[iIndex] != qProcess.front())
         {
-            answer.push_back(iCount);
-            break;
+            qProcess.push(qProcess.front());
+            qProcess.pop();
+            iLocationIndex = 0 == iLocationIndex ? qProcess.size() - 1 : iLocationIndex - 1;
         }
-        else if (iCheck >= qHandout.front())
-        {
-            ++iCount;
-            qHandout.pop();
-        }
+        // 내림차순으로 정렬된 순서와 큐의 제일 앞에 있는 수가 같다면 내림차순 정렬에서 확인하는 인덱스 증가
+        // 큐에서는 맨 앞 같은 수 제거
+        // 프로세스 실행 카운트 증가(++answer)
+        // 큐에서 제거하므로 위치 인덱스 또한 변형되므로 현재 위치 인덱스에서 -1
+        // 위치 인덱스가 0이면 제일 앞이라는 뜻. 즉 알고 싶은 위치 인덱스의 나가는 순서에 해당되므로 반복을 멈추고 나감
+        // 따라서 '큐에서 제거하므로~'의 주석에서 검토하는 것보다 앞의 위치에 있어 -1시키면 안되는 경우는 어쩌지? 라는 생각할 필요X
         else
         {
-            answer.push_back(iCount);
-            iCheck = qHandout.front();
-            qHandout.pop();
-            iCount = 1;
+            if (0 == iLocationIndex)
+            {
+                break;
+            }
+            ++iIndex;
+            qProcess.pop();
+            --iLocationIndex;
+            ++answer;
         }
     }
 
@@ -186,8 +236,12 @@ int main(void)
     solution({ {"crow_mask", "face"} ,{"blue_sunglasses", "face"},{"smoky_makeup", "face"} });*/
 
     // 기능개발
-    solution({ 93, 30, 55 }, { 1, 30, 5 });
-    solution({ 95, 90, 99, 99, 80, 99 }, { 1, 1, 1, 1, 1, 1 });
+    /*solution({ 93, 30, 55 }, { 1, 30, 5 });
+    solution({ 95, 90, 99, 99, 80, 99 }, { 1, 1, 1, 1, 1, 1 });*/
+
+    // 프로세스
+    solution({ 2, 1, 3, 2 }, 2);
+    solution({ 1, 1, 9, 1, 1, 1 }, 0);
 
     return 0;
 }
